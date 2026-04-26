e8beb48e8316ee09748abdc3b938564607c73565
Neural Nodes <neural.nodes.ai@gmail.com>
2026-04-26 21:47:29 +0530

    RELEASE V2.3.0

--- src/main/java/com/neuralnodes/inbox/SDKVersion.kt
+++ src/main/java/com/neuralnodes/inbox/SDKVersion.kt
@@ -7,7 +7,7 @@ import android.os.Build
  * Matches iOS SDKVersion implementation
  */
 object SDKVersion {
-    const val version = "2.2.9"
+    const val version = "2.3.0"
     
     const val name = "NeuralNodesInbox-Android"
     
